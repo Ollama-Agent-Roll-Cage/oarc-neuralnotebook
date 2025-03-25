bf47f29b135eb188c42582e8d212699776658ea7
Leo Borcherding <leoleojames1@gmail.com>
2025-03-25 01:59:40 -0500

    Removed unnecessary files

--- modules/main_script.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from neural_notebook_ui import NotebookApp
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = NotebookApp()
-    window.show()
-    sys.exit(app.exec())
